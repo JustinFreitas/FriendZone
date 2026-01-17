cec5d30731702773e140681e706c38e05f10a217
JustinFreitas <justin.s.freitas@me.com>
2026-01-17 16:25:24 -0700

    Changes for FGU to make FZ work.  Plus, many enhancements around CT sorting and other FZ functionality.

--- scripts/friendzone.lua
+++ scripts/friendzone.lua
@@ -15,7 +15,17 @@ function onInit()
 		AssistantGMManager.NotifyAddHolderOwnership = notifyAddHolderOwnership;
 	end
 	if Session.IsHost then
+		Comm.registerSlashHandler("subinit", CombatManagerFZ.processSubinitChatCommand);
+		Comm.registerSlashHandler("fzsync", processSyncCommand);
 		DB.addHandler("charsheet.*.level", "onUpdate", onLevelChanged)
+	end
+end
+
+function processSyncCommand(sCommand, sParams)
+	if CombatManagerFZ and CombatManagerFZ.syncAllCohorts then
+		CombatManagerFZ.syncAllCohorts(true);
+	else
+		ChatManager.SystemMessage("CombatManagerFZ.syncAllCohorts not found.");
 	end
 end
 
@@ -44,6 +54,86 @@ function addCohort(nodeChar, nodeNPC)
 	end
 
 	DB.copyNode(nodeNPC, nodeNewCohort);
+	
+	-- Prefix name with Commander's name and ensure uniqueness
+	local sCommanderName = DB.getValue(nodeChar, "name", "");
+	local sNPCName = DB.getValue(nodeNPC, "name", "");
+	local sBaseName = sNPCName;
+	if sCommanderName ~= "" and sNPCName ~= "" then
+		sBaseName = sCommanderName .. "'s " .. sNPCName;
+	end
+
+	local sFinalName = sBaseName;
+	
+	-- Check for exact match or existing numbered versions
+	local nodeOriginal = nil;
+	local bHasNumbered = false;
+	local sSafeBaseName = sBaseName:gsub("([%(%)%.%%%+%-%*%?%[%^%$])", "%%%1"); -- Escape magic chars
+
+	for _, vChild in pairs(DB.getChildren(nodeCohorts)) do
+		if vChild ~= nodeNewCohort then
+			local sChildName = DB.getValue(vChild, "name", "");
+			if sChildName == sBaseName then
+				nodeOriginal = vChild;
+			elseif sChildName:match("^" .. sSafeBaseName .. " %d+$") then
+				bHasNumbered = true;
+			end
+		end
+	end
+
+	if nodeOriginal then
+		-- Original exists. Rename to "... 1"
+		local sOriginalRename = sBaseName .. " 1";
+		
+		-- Ensure "Name 1" isn't magically taken (e.g. user manually named one "Ghoul 1" but left "Ghoul")
+		local bSafeToRename = true;
+		for _, vChild in pairs(DB.getChildren(nodeCohorts)) do
+			if vChild ~= nodeNewCohort and vChild ~= nodeOriginal then
+				if DB.getValue(vChild, "name", "") == sOriginalRename then
+					bSafeToRename = false;
+					break;
+				end
+			end
+		end
+		
+		if bSafeToRename then
+			DB.setValue(nodeOriginal, "name", "string", sOriginalRename);
+			bHasNumbered = true; -- Now we effectively have a numbered series
+		end
+	end
+
+	-- If we have a series (or just started one), find the next available number
+	if bHasNumbered or nodeOriginal then
+		local nSuffix = 1;
+		local bUnique = false;
+		
+		while not bUnique do
+			bUnique = true;
+			local sTestName = sBaseName .. " " .. nSuffix;
+			
+			for _, vChild in pairs(DB.getChildren(nodeCohorts)) do
+				if vChild ~= nodeNewCohort then
+					local sChildName = DB.getValue(vChild, "name", "");
+					if sChildName == sTestName then
+						bUnique = false; -- Name taken
+						break;
+					end
+				end
+			end
+			
+			if not bUnique then
+				nSuffix = nSuffix + 1;
+			else
+				sFinalName = sTestName;
+			end
+		end
+	else
+		-- No original, no series. Just use the base name.
+		sFinalName = sBaseName;
+	end
+
+	DB.setValue(nodeNewCohort, "name", "string", sFinalName);
+
 	-- TODO: For this, we'll need to override import/export to add/strip the values for that char.
 	DB.setValue(nodeNewCohort, "commandernodename", "string", nodeChar.getNodeName());
 	HpManagerFZ.updateNpcHitPoints(nodeNewCohort);
@@ -68,6 +158,7 @@ end
 
 function isCohort(vRecord)
 	local rActor = ActorManager.resolveActor(vRecord);
+	
 	if rActor and rActor.sCreatureNode and rActor.sCreatureNode:match("%.cohorts%.") then
 		return true;
 	end
--- scripts/manager_combat_fz.lua
+++ scripts/manager_combat_fz.lua
@@ -5,22 +5,180 @@
 
 local showTurnMessageOriginal;
 local centerOnTokenOriginal;
-local addNPCHelperOriginal;
 local addUnitOriginal;
+local onNPCPostAddOriginal;
+local addNPCHelperOriginal;
+local rollInitOriginal;
+local rollInit2Original;
+local rollEntryInitOriginal;
 
 function onInit()
+    if not CombatManager then
+        Debug.console("FZ ERROR: CombatManager not found. Aborting.");
+        return;
+    end
+
 	showTurnMessageOriginal = CombatManager.showTurnMessage;
 	CombatManager.showTurnMessage = showTurnMessage;
 
 	centerOnTokenOriginal = CombatManager.centerOnToken;
 	CombatManager.centerOnToken = centerOnToken;
+	
+	rollInitOriginal = CombatManager.rollInit;
+	CombatManager.rollInit = rollInitFZ;
+	
+	if CombatManager2 then
+		rollInit2Original = CombatManager2.rollInit;
+		CombatManager2.rollInit = rollInit2FZ;
+	end
+	
+	rollEntryInitOriginal = CombatManager.rollEntryInit;
+	CombatManager.rollEntryInit = rollEntryInitFZ;
 
-	addNPCHelperOriginal = CombatManager.addNPCHelper;
-	CombatManager.addNPCHelper = addNPCHelper;
+	if CombatRecordManager then
+		onNPCPostAddOriginal = CombatRecordManager.getRecordTypePostAddCallback("npc");
+		CombatRecordManager.setRecordTypePostAddCallback("npc", onNPCPostAdd);
+	else
+		addNPCHelperOriginal = CombatManager.addNPCHelper;
+		CombatManager.addNPCHelper = addNPCHelper;
+	end
+
+	onSortCompareOriginal = CombatManager.onSortCompare;
+	CombatManager.onSortCompare = onSortCompareFZ;
 
 	if CombatManagerKw then
 		addUnitOriginal = CombatManagerKw.addUnit;
 		CombatManagerKw.addUnit = addUnit;
+	end
+	
+	DB.addHandler("combattracker.list.*.initresult", "onUpdate", onInitResultChanged);
+	
+end
+
+function rollInitFZ(tCustom)
+    if rollInitOriginal then rollInitOriginal(tCustom); end
+    syncAllCohorts(false);
+end
+
+function rollInit2FZ(tCustom)
+    if rollInit2Original then rollInit2Original(tCustom); end
+    syncAllCohorts(false);
+end
+
+function syncAllCohorts(bVerbose)
+    if bVerbose then Debug.console("FZ: Starting Manual Sync (Optimized)..."); end
+    
+    -- Pass 1: Gather Commander Inits
+    local tCommanderInits = {};
+    for _,nodeCT in pairs(DB.getChildren(CombatManager.CT_LIST)) do
+        local sClass, sRecord = DB.getValue(nodeCT, "link");
+        if sRecord and type(sRecord) == "string" then
+            sRecord = sRecord:match("^%s*(.-)%s*$");
+            if sRecord ~= "" then
+                tCommanderInits[sRecord] = DB.getValue(nodeCT, "initresult", 0);
+            end
+        end
+    end
+    
+    -- Pass 2: Apply to Cohorts
+    for _,nodeCT in pairs(DB.getChildren(CombatManager.CT_LIST)) do
+        local sMyCommander = DB.getValue(nodeCT, "commandernodename", "");
+        if sMyCommander ~= "" then
+            sMyCommander = sMyCommander:match("^%s*(.-)%s*$");
+            
+            if tCommanderInits[sMyCommander] then
+                if bVerbose then Debug.console("FZ SYNC: " .. nodeCT.getName() .. " -> " .. tCommanderInits[sMyCommander]); end
+                DB.setValue(nodeCT, "initresult", "number", tCommanderInits[sMyCommander]);
+            else
+                 if bVerbose then Debug.console("FZ SKIP: " .. nodeCT.getName() .. " commander not found: " .. sMyCommander); end
+            end
+        end
+    end
+
+    if bVerbose then Debug.console("FZ: Sync Complete."); end
+end
+
+function rollEntryInitFZ(nodeEntry)
+    if rollEntryInitOriginal then rollEntryInitOriginal(nodeEntry); end
+    fixCohortInit(nodeEntry, false);
+end
+
+function hex_dump(str)
+    if not str then return "nil" end;
+    local len = string.len(str)
+    local hex = ""
+    for i = 1, len do
+        local ord = string.byte(str, i)
+        hex = hex .. string.format("%02X ", ord)
+    end
+    return hex
+end
+
+function fixCohortInit(nodeCohort, bVerbose)
+	local sMyCommanderNodeName = DB.getValue(nodeCohort, "commandernodename", "");
+	if sMyCommanderNodeName ~= "" then
+		sMyCommanderNodeName = sMyCommanderNodeName:match("^%s*(.-)%s*$");
+		
+		if bVerbose then Debug.console("FZ CHECK: " .. nodeCohort.getName() .. " seeking " .. sMyCommanderNodeName); end
+		
+		for _,nodeCT in pairs(DB.getChildren(CombatManager.CT_LIST)) do
+			local sClass, sRecord = DB.getValue(nodeCT, "link");
+			if sRecord and type(sRecord) == "string" then 
+				sRecord = sRecord:match("^%s*(.-)%s*$"); 
+				
+				if sRecord == sMyCommanderNodeName then
+					local nCommanderInit = DB.getValue(nodeCT, "initresult", 0);
+					if bVerbose then Debug.console("   MATCH: Found Commander (" .. ActorManager.getDisplayName(nodeCT) .. ") Init: " .. nCommanderInit); end
+					DB.setValue(nodeCohort, "initresult", "number", nCommanderInit);
+					return;
+				end
+			end
+		end
+		
+		if bVerbose then Debug.console("   FAIL: Commander not found."); end
+	end
+end
+
+function onNPCPostAdd(tCustom)
+	if onNPCPostAddOriginal then
+		onNPCPostAddOriginal(tCustom);
+	end
+
+	processCohort(tCustom.nodeCT, tCustom.nodeRecord);
+end
+
+function addNPCHelper(nodeNPC, sName)
+	local nodeEntry, nodeLastMatch = addNPCHelperOriginal(nodeNPC, sName);
+	if nodeEntry then
+		processCohort(nodeEntry, nodeNPC);
+	end
+	return nodeEntry, nodeLastMatch;
+end
+
+function processCohort(nodeCT, nodeRecord)
+	if nodeRecord and nodeCT and FriendZone.isCohort(nodeRecord) then
+		DB.setValue(nodeCT, "link", "windowreference", "npc", nodeRecord.getPath());
+		
+		local sCommanderNode = DB.getValue(nodeRecord, "commandernodename", "");
+		DB.setValue(nodeCT, "commandernodename", "string", sCommanderNode);
+
+		setCohortFaction(nodeCT);
+		addCohortOfEffectIfEnabled(nodeCT);
+		
+		-- [NEW] Initial Sync: If commander is already in CT, snap to them.
+		if sCommanderNode ~= "" then
+			sCommanderNode = sCommanderNode:match("^%s*(.-)%s*$"); -- TRIM
+			for _,nodeExistingCT in pairs(DB.getChildren(CombatManager.CT_LIST)) do
+				local sClass, sRecord = DB.getValue(nodeExistingCT, "link");
+				if sRecord then sRecord = sRecord:match("^%s*(.-)%s*$"); end -- TRIM
+				
+				if sRecord and sRecord == sCommanderNode then
+					local nCommanderInit = DB.getValue(nodeExistingCT, "initresult", 0);
+					DB.setValue(nodeCT, "initresult", "number", nCommanderInit);
+					break;
+				end
+			end
+		end
 	end
 end
 
@@ -52,17 +210,6 @@ function centerOnToken(nodeEntry, bOpen)
 	DB.isOwner(ActorManager.getCreatureNode(nodeEntry)) then
 		ImageManager.centerOnToken(CombatManager.getTokenFromCT(nodeEntry), bOpen);
 	end
-end
-
-function addNPCHelper(nodeNPC, sName)
-	local bIsCohort = FriendZone.isCohort(nodeNPC);
-	local nodeEntry, nodeLastMatch = addNPCHelperOriginal(nodeNPC, sName);
-	if nodeEntry and bIsCohort then
-		DB.setValue(nodeEntry, "link", "windowreference", "npc", nodeNPC.getPath());
-		setCohortFaction(nodeEntry);
-		addCohortOfEffectIfEnabled(nodeEntry);
-	end
-	return nodeEntry, nodeLastMatch;
 end
 
 function setCohortFaction(nodeCohort)
@@ -106,4 +253,135 @@ function addUnit(sClass, nodeUnit, sName)
 	end
 
 	return nodeEntry;
+end
+
+function processSubinitChatCommand()
+	local tChildren = DB.getChildren(CombatManager.CT_LIST);
+
+	-- Strategy 1: Name Regex (Legacy/Fallback)
+	for _,nodeCT in pairs(tChildren) do
+		local sCharacterName = ActorManager.getDisplayName(nodeCT);
+		local sMsgSafeName = sCharacterName:gsub("([%^%$%(%)%%%.%[%]%*%+%-%?])", "%%%1");
+		local sRegex = "^" .. sMsgSafeName .. "'s?.*$";
+		local nNodeCTInit = DB.getValue(nodeCT, "initresult", 0);
+
+		for _,subNodeCT in pairs(tChildren) do
+			if nodeCT ~= subNodeCT then
+				local sSubName = ActorManager.getDisplayName(subNodeCT);
+				if string.match(sSubName, sRegex) then
+					DB.setValue(subNodeCT, "initresult", "number", nNodeCTInit);
+				end
+			end
+		end
+	end
+
+	-- Strategy 2: DB Link (Authoritative)
+	local tRecordInits = {};
+	for _,nodeCT in pairs(tChildren) do
+		local sClass, sRecord = DB.getValue(nodeCT, "link", "", "");
+		if sRecord ~= "" then
+			tRecordInits[sRecord] = DB.getValue(nodeCT, "initresult", 0);
+		end
+	end
+
+	for _,nodeCT in pairs(tChildren) do
+		local sCommanderNode = DB.getValue(nodeCT, "commandernodename", "");
+		if sCommanderNode ~= "" and tRecordInits[sCommanderNode] then
+			DB.setValue(nodeCT, "initresult", "number", tRecordInits[sCommanderNode]);
+		end
+	end
+
+	ChatManager.SystemMessage("Sub-initiatives processed.");
+end
+
+local bUpdating = false;
+
+function onInitResultChanged(nodeField)
+	if bUpdating then return; end
+
+	local nodeCommander = nodeField.getParent();
+	local nNewInit = nodeField.getValue();
+	
+	-- Identify Commander Link
+	local sClass, sRecord = DB.getValue(nodeCommander, "link");
+	if sRecord then sRecord = sRecord:match("^%s*(.-)%s*$"); end -- TRIM
+	
+	-- Identify Commander Name (for regex)
+	local sCharacterName = ActorManager.getDisplayName(nodeCommander);
+	local sMsgSafeName = sCharacterName:gsub("([%^%$%(%)%%%.%[%]%*%+%-%?])", "%%%1");
+	local sRegex = "^" .. sMsgSafeName .. "'s?.*$";
+	
+	-- Iterate all CT nodes to find cohorts
+	for _,nodeCT in pairs(DB.getChildren(CombatManager.CT_LIST)) do
+		if nodeCT ~= nodeCommander then
+			local bMatch = false;
+			
+			-- Check Link (Authoritative)
+			local sCohortCommander = DB.getValue(nodeCT, "commandernodename", "");
+			if sCohortCommander then sCohortCommander = sCohortCommander:match("^%s*(.-)%s*$"); end -- TRIM
+			
+			if sCohortCommander and sCohortCommander ~= "" and sCohortCommander == sRecord then
+				bMatch = true;
+			end
+			
+			-- Check Regex (Fallback)
+			if not bMatch then
+				local sSubName = ActorManager.getDisplayName(nodeCT);
+				if string.match(sSubName, sRegex) then
+					bMatch = true;
+				end
+			end
+			
+			if bMatch then
+				if DB.getValue(nodeCT, "initresult", 0) ~= nNewInit then
+					bUpdating = true;
+					DB.setValue(nodeCT, "initresult", "number", nNewInit);
+					bUpdating = false;
+				end
+			end
+		end
+	end
+end
+
+function onSortCompareFZ(node1, node2)
+	local nInit1 = DB.getValue(node1, "initresult", 0);
+	local nInit2 = DB.getValue(node2, "initresult", 0);
+	
+	if nInit1 == nInit2 then
+		local sCmdr1 = DB.getValue(node1, "commandernodename", "");
+		local sCmdr2 = DB.getValue(node2, "commandernodename", "");
+		
+		-- TRIM
+		if sCmdr1 then sCmdr1 = sCmdr1:match("^%s*(.-)%s*$"); end
+		if sCmdr2 then sCmdr2 = sCmdr2:match("^%s*(.-)%s*$"); end
+		
+		local sClass1, sRecord1 = DB.getValue(node1, "link");
+		local sClass2, sRecord2 = DB.getValue(node2, "link");
+		
+		-- TRIM
+		if sRecord1 then sRecord1 = sRecord1:match("^%s*(.-)%s*$"); end
+		if sRecord2 then sRecord2 = sRecord2:match("^%s*(.-)%s*$"); end
+		
+		-- Node1 is Commander of Node2? (Check Node2's commander against Node1's link)
+		if sCmdr2 and sCmdr2 ~= "" and sCmdr2 == sRecord1 then
+			return false; -- Node1 (Commander) comes first
+		end
+		
+		-- Node2 is Commander of Node1? (Check Node1's commander against Node2's link)
+		if sCmdr1 and sCmdr1 ~= "" and sCmdr1 == sRecord2 then
+			return true; -- Node2 (Commander) comes first
+		end
+	end
+	
+	if onSortCompareOriginal then
+		return onSortCompareOriginal(node1, node2);
+	end
+
+	-- Fallback default sort if no original function (Init Desc > Name Asc)
+	if nInit1 ~= nInit2 then
+		return nInit1 > nInit2;
+	end
+	local sName1 = DB.getValue(node1, "name", "");
+	local sName2 = DB.getValue(node2, "name", "");
+	return sName1 < sName2;
 end
